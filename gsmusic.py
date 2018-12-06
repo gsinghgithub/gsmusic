4d9dd9afbd5ce6342b63bd772fc52fce37c61cc9
Gopal P Singh <gopalpsingh@Gopals-MacBook-Pro-2.local>
2018-12-05 21:59:56 -0800

    .

--- gsmusic.py
+++ gsmusic.py
@@ -1,1 +1,15 @@
-
+#!/usr/bin/env python
+from GsMidiFile import MIDIFile
+from GsMidiFile import TICKSPERBEAT_CONFIG
+import re
+import struct
+import os
+import logging
+import logging.handlers
+import time
+from time import gmtime, strftime
+print strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+logger = logging.getLogger(__name__)
+LOGFILE = 'log_midi.txt'
+logger.setLevel(logging.DEBUG)
+t = time.localtime()
